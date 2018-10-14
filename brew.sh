0f3d0d3b21df644e61c95ef564fba0fff017bd01
Agustinus Verdy Widyawiradi <verdywid@gmail.com>
2018-10-14 17:13:09 +0700

    Add basic cli utils through homebrew

--- brew.sh
+++ brew.sh
@@ -5,6 +5,18 @@
 brew update
 brew upgrade
 
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
 brew install 'openssl'
 brew install 'qt5'
 brew install 'imagemagick'
