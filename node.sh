efdf2f2a70e9e40233503bffbd18ccc9a02269dd
Verdy <verdy@sleekr.co>
2019-05-27 11:09:30 +0700

    Install yarn through install script instead of brew

--- node.sh
+++ node.sh
@@ -7,11 +7,11 @@ npm install -g n
 # Install the latest stable version
 n stable
 
-brew install yarn --without-node
+curl -o- -L https://yarnpkg.com/install.sh | bash
 
 # Needed for react native
 brew install 'watchman'
-# Either using expo 
+# Either using expo
 npm install -g expo-cli
 # Or react-native cli
 npm install -g react-native-cli
