e1e23e1d205491003549c761f6ec66b9453acc39
Agus <agustinus_widyawiradi@mckinsey.com>
2024-12-28 01:32:02 +0700

    Modularize tmux installation and config

---
+++ tmux/main.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminal multiplexer
+brew install 'tmux'
+## Fix paste mechanism under tmux on mac
+brew install 'reattach-to-user-namespace'
+
+cp .tmux.conf ~/.tmux.conf
+
+./plugins.sh
